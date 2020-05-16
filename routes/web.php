5ceb560573af51701cc2e13a5236356d083ca1d8
Naufal <naufalmahfudzismail@gmail.com>
2020-05-16 13:31:40 +0700

    little bug

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('dashboard/surat');
+    return view('dashboard/surat/index');
 });
 
 //Route::get('/admin', 'AdminController@index');
