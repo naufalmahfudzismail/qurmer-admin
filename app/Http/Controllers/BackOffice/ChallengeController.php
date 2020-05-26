93a403729904e91e2dfc27ee04b540317e40e181
Naufal <naufalmahfudzismail@gmail.com>
2020-05-26 09:20:14 +0700

    start challenge

--- app/Http/Controllers/BackOffice/ChallengeController.php
+++ app/Http/Controllers/BackOffice/ChallengeController.php
@@ -34,8 +34,8 @@ class ChallengeController extends Controller
 
     public function getData(){
         $data = Challenge::select('challenges.*', 'surahs.nama', 'levels.id', 'levels.name',
-         '(levels.bonus_score + challenges.score) AS point')->join('levels', 'challenges.level_id', 'levels.id')
-        ->join('surahs', 'challenges.surah_id', 'surahs.id')->get();
+         '(levels.bonus_score + challenges.score) AS point')->leftjoin('levels', 'challenges.level_id', 'levels.id')
+        ->leftjoin('surahs', 'challenges.surah_id', 'surahs.id')->get();
 
         return DataTables::of($data)
             ->addIndexColumn()
