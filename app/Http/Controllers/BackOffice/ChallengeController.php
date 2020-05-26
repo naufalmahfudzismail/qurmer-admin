b2a67ed26a21c5e80db850a267c3b12301846da1
Naufal <naufalmahfudzismail@gmail.com>
2020-05-26 08:34:19 +0700

    start challenge

--- app/Http/Controllers/BackOffice/ChallengeController.php
+++ app/Http/Controllers/BackOffice/ChallengeController.php
@@ -33,7 +33,7 @@ class ChallengeController extends Controller
 
     public function getData(){
         $data = Challenge::join('levels', 'challenges.level_id', 'levels.id')
-        ->join('surahs', 'challengs.surah_id', 'surahs.id')->get();
+        ->join('surahs', 'challenges.surah_id', 'surahs.id')->get();
 
         dd($data);
 
