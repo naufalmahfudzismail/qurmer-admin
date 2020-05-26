b53420c9792d9796899d4ef412be7a330d350dbf
Naufal <naufalmahfudzismail@gmail.com>
2020-05-26 08:36:26 +0700

    start challenge

--- app/Http/Controllers/BackOffice/ChallengeController.php
+++ app/Http/Controllers/BackOffice/ChallengeController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Level;
 use App\Model\Challenge;
 use App\Model\Surah;
+use Yajra\DataTables\Facades\DataTables;
 
 class ChallengeController extends Controller
 {
