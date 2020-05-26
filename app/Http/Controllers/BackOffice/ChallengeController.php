2079b7c5d4e5a23d08ef843c712b3528cd3e8a92
Naufal <naufalmahfudzismail@gmail.com>
2020-05-26 08:38:57 +0700

    start challenge

--- app/Http/Controllers/BackOffice/ChallengeController.php
+++ app/Http/Controllers/BackOffice/ChallengeController.php
@@ -36,7 +36,7 @@ class ChallengeController extends Controller
         $data = Challenge::join('levels', 'challenges.level_id', 'levels.id')
         ->join('surahs', 'challenges.surah_id', 'surahs.id')->get();
         dd($data);
-        return DataTables::of($data)
+        /*return DataTables::of($data)
             ->addIndexColumn()
             ->addColumn('action', function ($row) {
                 $button = '<a href="/master/article/' . $row['id'] . '/edit"><button class="btn btn-warning btn-sm edit" style="float:left;" id="' . $row['id'] . '"><i class="fa fa-pencil"></i> Edit</button></a>';
@@ -44,7 +44,7 @@ class ChallengeController extends Controller
                 return $button;
             })
             ->rawColumns(['action'])
-            ->make(true);
+            ->make(true);*/
     }
 
     /**
