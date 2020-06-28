264f040f12873a62cba2e176472426d5103360bc
Naufal <naufalmahfudzismail@gmail.com>
2020-06-28 21:21:03 +0700

    capek w asli

--- app/Http/Controllers/API/HistoryController.php
+++ app/Http/Controllers/API/HistoryController.php
@@ -17,9 +17,7 @@ class HistoryController extends Controller
             $data = History::where('activity_name', 'progress')->where('user_id', Auth::user()->id)->get();
             foreach($data as $dt){
                 $dt['progress'] = Progress::find($dt->activity_id)->with(
-                    'challenge',
-                    'challenge.level',
-                    'challenge.surah'
+                    'challenge'
                 );
             }
             return SendResponse::success($data, 200);
