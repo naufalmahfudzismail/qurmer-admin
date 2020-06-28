5b02850625b63a9976376548c4d0966bc039192c
Naufal <naufalmahfudzismail@gmail.com>
2020-06-28 21:34:47 +0700

    capek w asli

--- app/Http/Controllers/API/HistoryController.php
+++ app/Http/Controllers/API/HistoryController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Model\History;
-use App\Model\Progress;
 use App\Services\SendResponse;
 use Auth;
 
@@ -14,14 +13,13 @@ class HistoryController extends Controller
     public function getAllHistory()
     {
         try {
-            $data = History::where('activity_name', 'progress')->where('user_id', Auth::user()->id)->get();
-            foreach($data as $dt){
-                $dt['progress'] = Progress::find((int)$dt->activity_id)->with(
-                    'challenge',
-                    'challenge.level',
-                    'challenge.surah'
-                );
-            }
+            $data = History::where('activity_name', 'progress')->where('user_id', Auth::user()->id)
+                ->with(
+                    'progress',
+                    'progress.challenge',
+                    'progress.challenge.surah',
+                    'progress.challenge.level'
+                )->get();
             return SendResponse::success($data, 200);
         } catch (\Exception $e) {
             return SendResponse::fail('Gagal, karena: ' . $e->getMessage(), 500);
