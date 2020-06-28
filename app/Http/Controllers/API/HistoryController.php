8233a40adaa060be759e0ae694c6c62cd3bc67b6
Naufal <naufalmahfudzismail@gmail.com>
2020-06-28 20:32:33 +0700

    capek w asli

--- app/Http/Controllers/API/HistoryController.php
+++ app/Http/Controllers/API/HistoryController.php
@@ -13,7 +13,13 @@ class HistoryController extends Controller
     public function getAllHistory()
     {
         try {
-            $data = History::with('user')->where('user_id', Auth::user()->id)->with('progress', 'progress.challenge', 'progress.challenge.surah'. 'progress.challenge.level')->get();
+            $data = History::with('user')->where('user_id', Auth::user()->id)
+                ->with(
+                    'progress',
+                    'progress.challenge',
+                    'progress.challenge.surah',
+                    'progress.challenge.level'
+                )->get();
             return SendResponse::success($data, 200);
         } catch (\Exception $e) {
             return SendResponse::fail('Gagal, karena: ' . $e->getMessage(), 500);
